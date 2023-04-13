b09edd20e559b8fe9805bbe16292cac49b96b5ee
rilvald <129185166+rilvald@users.noreply.github.com>
2023-04-14 00:21:30 +0700

    Update and rename list_news.dart to newa_dummy.dart
    mengganti struktur syntax  dan merubah nama file dari list_news menjadi news_dummy

---
+++ lib/models/newa_dummy.dart
@@ -0,0 +1,96 @@
+import './news.dart';
+
+News hotesNewsDummy = News(
+  id: 'hotes-001',
+  date: '13-04-2023',
+  imageUrl: 'https://i.postimg.cc/pTKyX4VS/coverberita.jpg',
+  title: 'Temukan Berbagai Berita Disekitar Anda',
+  content:
+      'Temukan berbagai berita disekitar anda untuk menambah informasi dan wawasan anda, kami selalu menampilkan berita yang uptodate yang sedang viral, panas, dan trendi. terus ikuti kami :)      Project ini dibuat untuk memenuhi salah satu Tugas Kelompok 6 dari MataKuliah Praktikum Pemrograman Mobile dengan anggota - Moh Rival dzulkhomzah (2006114), - Shelly umayah fhadillah (2006104), -Rida Sofiatuhusna (2006097), - Reifalga gais Pratama (2006055)',
+);
+
+List<News> latesNewsDummy = [
+  News(
+    id: 'lates-002',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/rms0gKQ1/1.webp',
+    title: 'Mahasiswa ITG Sedang Mengerjakan Kegiatan Kerja Kelompok',
+    content:
+        'Mahasiswa ITG angkatan 2020 kelas C dari Teknik Informatika sedang melakukan kegiatan kerja kelompok untuk memenuhi salah satu tugas dari Praktikum Pemrograman Mobile di sekitar kampus ITG. meskipun sedang melaksanakan ibadah puasa, antusias mahasiswa tidak berkurang sedikitpun untuk melakukan kegiatan kerja kelompok karena sudah makan dan minum awokwk',
+  ),
+  News(
+    id: 'lates-003',
+    date: '03-04-2023',
+    imageUrl: 'https://i.postimg.cc/Jnh4ctyL/berita3.jpg',
+    title: 'jalan Dilampung Sangat parah!!!',
+    content:
+        'Jalan raya sudah ada sejak manusia memerlukan area untuk berjalan, terlebih-lebih setelah menemukan kendaraan beroda diantaranya berupa kereta yang ditarik kuda. Tidak jelas dikatakan bahwa peradaban mana yang lebih dahulu membuat jalan raya. Akan tetapi hampir semua peradaban tidak terlepas dari keberadaan jalan raya tersebut. kondisi jalan raya di lampung sangat memperihatinkan dikarnakan jalan adalah salah satu faktor yang menopang perekonomian. warga lampung berharap segera ada perhatian dari pemerintahan terkait',
+  ),
+  News(
+    id: 'lates-004',
+    date: '04-04-2023',
+    imageUrl: 'https://i.postimg.cc/vZNpSzZD/foto-bukber.jpg',
+    title:
+        'Mahasiswa Teknik Informatika C angkatan 2020 Melakukan Kegiatan Bukber Di Lumbung Padi',
+    content:
+        'Bukber adalah singkatan dari "buka bersama", sebuah tradisi umat Muslim yang dilakukan pada bulan Ramadan. Bukber biasanya dilakukan saat waktu berbuka puasa tiba, yaitu pada saat matahari terbenam. Acara bukber biasanya diawali dengan doa bersama untuk membuka puasa. Kemudian, hidangan makanan dan minuman disajikan untuk berbuka puasa. Makanan yang disajikan biasanya berupa makanan tradisional atau makanan ringan yang lezat dan menyegarkan, seperti kolak, kurma, bubur kacang hijau, minuman es, dan lain sebagainya. Kegiatan bukber kali ini dilakukan oleh kelas C teknik Informatika 2020 yang bertempat di rumah makan Lumbung Padi.',
+  ),
+  News(
+    id: 'lates-054',
+    date: '05-04-2023',
+    imageUrl: 'https://i.postimg.cc/9FyLQ2xP/Foto-Jet-8-2797139411.webp',
+    title:
+        'Mahasiswa Itg T.I Angkatan 2020 Sedang Melakukan Survey Tempat Untuk Bukber',
+    content:
+        'Garut merupakan salah satu kabupaten di Jawa Barat yang menyajikan berbagai tempat yang bisa dijadikan tempat buka bersama (bukber). Terdapat beberapa tempat buka puasa bersama (bukber) yang bisa kamu kunjungi di Garut. Beberapa di antaranya adalah Kampung Sampireun Resort & Spa, The Patra Garut Hotel, Sumber Alam Resort, Dbest Hotel, dan Saung Alam Sutera.',
+  ),
+  News(
+    id: 'lates-005',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/kgtK3Jy9/berita5.jpg',
+    title: 'Laptop Asus Tuf F15 banting harga!!!',
+    content:
+        'Laptop ASUS TUF F15 adalah laptop gaming yang dirancang untuk memberikan performa terbaik dan ketangguhan yang diperlukan dalam permainan modern. Laptop ini dilengkapi dengan prosesor Intel Core i7 generasi ke-11, kartu grafis NVIDIA GeForce RTX 3060, dan memori RAM 16GB DDR4. Layar laptop ASUS TUF F15 berukuran 15,6 inci dengan resolusi Full HD 1080p dan refresh rate 144Hz, sehingga memberikan tampilan yang jernih dan responsif saat bermain game. Selain itu, laptop ini juga dilengkapi dengan sistem pendingin yang efektif untuk menjaga suhu tetap rendah saat laptop digunakan dalam waktu yang lama. harga laptop ASUS TUF F15 dapat berkisar antara sekitar USD 1.200 hingga USD 1.500 atau lebih',
+  ),
+  News(
+    id: 'lates-006',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/rsmgS62m/berita6.jpg',
+    title:
+        'Seorang Pemuda Yang Tidur Siang Mendapat Spoiler Kehidupan Di Akhirat!!!',
+    content:
+        'Seorang pemuda berusia 20tahun yang sedang tidur siang tapi tidak puasa dibulan Ramadhan mendapat mimpi yang aneh. ia mengaku sedang disiksa di akhirat. pemuda tersebut lantas bergegas mengambil air wudhu lalu melanjutkan tidur siangnya.',
+  ),
+  News(
+    id: 'lates-007',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/tTyXkjk9/berita7.jpg',
+    title: 'Kucing Hilang!!!',
+    content:
+        'Kucing hilang adalah situasi ketika kucing peliharaan yang biasanya tinggal di rumah atau di lingkungan sekitar tiba-tiba tidak terlihat atau tidak kembali lagi ke rumah dalam waktu yang lama. Kucing dapat hilang karena berbagai alasan, seperti tersesat saat menjelajahi lingkungan, mencari makanan atau tempat berteduh, atau bahkan karena diculik oleh orang yang tidak bertanggung jawab. Kucing yang hilang dapat merasakan rasa ketakutan dan kecemasan yang besar, dan dapat mencari tempat yang aman untuk bersembunyi atau mencari makanan dan minuman. Kucing juga dapat melintasi jalan dan terkena bahaya, seperti terkena kendaraan atau diserang oleh hewan lain. jika ada yang menemukan kucing seperti pada gambar dapat menghubungi No.HP 083774629487  terimakasih.',
+  ),
+  News(
+    id: 'lates-008',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/FH8b7PZy/berita8.jpg',
+    title: 'MUKENA MERAH!!!',
+    content:
+        'Kemarin sempat heboh video seorang wanita yang mengenakan kebaya mearah, sekarang ada lagi video Mukena merah!!!! dikutip dari twitter banyak netizen yang memperbincangkan mukena merah tapi setelah di telusuri berita itu hanyalah bohong. berita itu adalah hasil imajinasi programmer untuk menambah konten berita wkwkw',
+  ),
+  News(
+    id: 'lates-009',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/7YM0xHGy/berita9.jpg',
+    title: 'AZAB!! WARUNG NASI?!',
+    content:
+        'Warung nasi adalah jenis usaha kuliner yang sangat populer di Indonesia. Warung nasi biasanya merupakan usaha skala kecil yang menyediakan hidangan nasi dan lauk-pauk, serta minuman dan camilan kecil. Warung nasi biasanya terletak di pinggir jalan atau di pasar tradisional. Warung nasi biasanya menjadi tempat makan yang populer karena menyediakan makanan yang murah, lezat, dan mudah ditemukan. Warung nasi juga menjadi tempat berkumpul dan bersantai bagi banyak orang, terutama pada waktu makan siang atau makan malam. Banyak orang Indonesia menganggap warung nasi sebagai salah satu simbol keberagaman kuliner Indonesia yang kaya dan unik.',
+  ),
+  News(
+    id: 'lates-010',
+    date: '02-04-2023',
+    imageUrl: 'https://i.postimg.cc/V6C4gx7L/berita10.jpg',
+    title: 'Recomendasi Baju Lebaran Untuk Wanita Tahun 2023',
+    content:
+        'Baju wanita dapat beragam jenis dan model, tergantung pada jenis acara atau kegiatan yang akan dihadiri. Baju wanita juga dapat dirancang dengan berbagai variasi warna, motif, dan dekorasi seperti renda, bordir, dan aplikasi. Baju wanita menjadi salah satu aspek penting dalam penampilan perempuan karena dapat memberikan kesan yang berbeda-beda tergantung pada model dan jenis acara yang akan dihadiri. berikut adalah contoh baju yang direkomendasikan oleh admin untuk Lebaran Tahun 2023',
+  ),
+];

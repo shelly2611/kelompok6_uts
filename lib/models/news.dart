2ddd7567d7ed8226c01dff8acbb12ce98851ae0b
rilvald <129185166+rilvald@users.noreply.github.com>
2023-04-14 00:23:10 +0700

    Update news.dart
    menambahkan variabel untuk tanggal

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -1,11 +1,13 @@
 class News {
   final String id;
+  final String date;
   final String imageUrl;
   final String title;
   final String content;
 
   News({
     required this.id,
+    required this.date,
     required this.imageUrl,
     required this.title,
     required this.content,

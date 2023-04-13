2ac2423570c0db1db56515cf66f6fb5b25f73c10
Ridasofiatuhusna <2006097@itg.ac.id>
2023-04-13 11:14:37 +0700

    Menambah List News_Rubah berita nya di list news yaa

---
+++ lib/models/news.dart
@@ -0,0 +1,13 @@
+class News {
+  final String id;
+  final String imageUrl;
+  final String title;
+  final String content;
+
+  News({
+    required this.id,
+    required this.imageUrl,
+    required this.title,
+    required this.content,
+  });
+}

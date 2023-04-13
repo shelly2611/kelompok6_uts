0bf4827749a9d2258ae01945cff4234f5008f664
rilvald <129185166+rilvald@users.noreply.github.com>
2023-04-14 00:32:15 +0700

    Update news_detail_screen.dart
    menambahkan beberapa syntax

--- lib/modules/news_detail_screen/news_detail_screen.dart
+++ lib/modules/news_detail_screen/news_detail_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pertemuan_v/models/news.dart';
 
 class NewsDetailScreen extends StatefulWidget {
   const NewsDetailScreen({
     super.key,
-    required this.id,
+    required this.news,
   });
-  final String id;
+  final News news;
 
   @override
   State<NewsDetailScreen> createState() => _NewsDetailScreenState();
@@ -15,9 +16,54 @@ class _NewsDetailScreenState extends State<NewsDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          widget.id,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: MediaQuery.of(context).padding.top),
+            Image.network(
+              widget.news.imageUrl,
+              // fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.news.date,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    widget.news.title,
+                    maxLines: 100,
+                    textAlign: TextAlign.start,
+                    style: const TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    widget.news.content,
+                    maxLines: 100,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
